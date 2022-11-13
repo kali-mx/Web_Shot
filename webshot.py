84268195104928b6ed42cb6372cffeaa3b9b6835
Max Ahartz <76034874+kalimax69@users.noreply.github.com>
2022-11-13 14:39:33 -0800

    Update webshot.py

--- webshot.py
+++ webshot.py
@@ -1,22 +1,21 @@
+# Author: Max Ahartz
+# Created: 11-13-22
+# Inspired by: Ebram Rezk
 
 from selenium import webdriver
 from time import sleep
 import os,sys
 
-# Author: Max Ahartz
-# Created: 11-13-22
-# Inspired by: Ebram Rezk
-
-url = input ('Enter target IP. Example:http://10.10.185.20 -->')
-
-gobust = "gobuster dir -u " + url + " -w /usr/share/seclists/Discovery/Web-Content/raft-medium-files.txt -k -t 30 -o dirnames.txt"
+url = input ('Enter target IP.\nExample:http://10.10.185.20 -->')
+word_list = "/usr/share/seclists/Discovery/Web-Content/raft-medium-files.txt"
+gobust = "gobuster dir -u " + url + " -w " + word_list + " -k -t 100 -o dirnames.txt"
 os.system(gobust)
 os.system("grep ': 200' dirnames.txt | cut -d ' ' -f 1 | cut -d '/' -f 2 > names.txt")
 
 driver = webdriver.Firefox()
 
-with open('names.txt', 'r') as f:
-    for name in f:
+with open('names.txt', 'r') as file:
+    for name in file:
         uri = url + "/" + name
         print(uri)
         driver.get(uri)
@@ -25,8 +24,7 @@ with open('names.txt', 'r') as f:
         picture = "screenshot_" + name.replace("\n", "") + ".png"
         print(picture)
         driver.get_screenshot_as_file(picture)
-f.close()        
+file.close()        
 driver.quit()
 
 os.system("ristretto screenshot_*") #show me the pics!
-quit()
